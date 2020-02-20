99cb87b2849b47a2b40f0fe7665afec0f7fb3753
Anheru88 <ajimenez@digitalcaribe.com.co>
2020-02-20 00:37:52 -0500

    changes in dockerfile and beggining of rabbitmq recibe file

---
+++ htcondor/recibe.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import pika, json, subprocess
+
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='rabbitmq'))
+channel = connection.channel()
+
+channel.queue_declare(queue='nextcloud')
+
+def callback(ch, method, properties, body):
+    body = json.loads(body)
+    file = body['file'].replace("/var/www/html/", "/home/nextcloud/")
+    p = subprocess.Popen(["condor_submit", file], stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE,
+                                 stdin=subprocess.PIPE)
+    out, err = p.communicate()
+    print(file)
+
+channel.basic_consume(
+    queue='nextcloud', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
