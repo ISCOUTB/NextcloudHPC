d16b535ee1984fd48a05a3d7607f54703b6ffe39
Anheru88 <ajimenez@digitalcaribe.com.co>
2020-02-20 11:38:09 -0500

    Fix problem with path when run condor_submit

--- htcondor/recibe.py
+++ htcondor/recibe.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import pika, json, subprocess
 
-
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
@@ -11,11 +10,19 @@ channel.queue_declare(queue='nextcloud')
 def callback(ch, method, properties, body):
     body = json.loads(body)
     file = body['file'].replace("/var/www/html/", "/home/nextcloud/")
-    p = subprocess.Popen(["condor_submit", file], stdout=subprocess.PIPE,
+    split = file.split("/")
+    count = len(split)
+    path = ""
+    for x in range (count - 1):
+        path += split[x] + "/"
+
+    p = subprocess.Popen(["condor_submit", split[count - 1]], cwd=path,
+                                 stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE,
                                  stdin=subprocess.PIPE)
     out, err = p.communicate()
     print(file)
+
 
 channel.basic_consume(
     queue='nextcloud', on_message_callback=callback, auto_ack=True)
