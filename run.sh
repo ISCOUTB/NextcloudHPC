54bf162e25d4c04f17da013ea5c0fe1803901da1
anheru88 <ajimenez@digitalcaribe.com.co>
2020-05-03 06:55:21 -0500

    Cambio en el archivo run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-docker-compose up -d && docker-compose exec -u www-data htcondor nohup bash -c "python -u /home/www-data/recibe.py > /home/www-data/recibe.out &"
+dot="$(cd "$(dirname "$0")"; pwd)"
+path1="$dot/temp/condorsubmit"
+path2="$path1/vendor"
+path3="$dot/nextcloud/custom_apps/condorsubmit"
+
+if [ ! -d $path1 ]
+then
+    git clone https://github.com/anheru88/condorsubmit.git temp/condorsubmit
+fi
+
+if [ ! -d $path2 ]
+then
+    (cd $path1 && composer install)
+fi
+
+docker-compose up -d
+
+sleep 10s
+docker-compose exec -u www-data htcondor nohup bash -c "python -u /home/www-data/recibe.py > /home/www-data/recibe.out &"
+
+if [ ! -d $path3 ]
+then
+    (cp -r "$dot/temp/condorsubmit" $path3)
+fi
